374a16339dcd10258ba060da6f1e2a75681f931b
MUSKANNISHAD <mkumari40432@gmail.com>
2026-09-16 22:19:06 +0530

    typo error

--- Dashboard/src/Components/Home.jsx
+++ Dashboard/src/Components/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Dashboard from './dashboard';
+import Dashboard from './Dashboard.jsx';
 import Topbar from './Topbar.jsx';
 import { useEffect } from "react";
 import { ClientServer } from '../ClientServer';
